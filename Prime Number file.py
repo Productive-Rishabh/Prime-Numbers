019df3d6fb0db3e25417a051008e1fb7f68e3b07
Rishabh Mehta <77108851+Productive-Rishabh@users.noreply.github.com>
2021-02-10 23:10:49 +0530

    Update Prime Number file.py

--- Prime Number file.py
+++ Prime Number file.py
@@ -1,1 +1,19 @@
+# Prime Number not above 1000
 
+x=int(input("Enter Your Number: "))
+
+
+for i in range (2,x):
+     if x%i==0 and x<1000:
+        print(x,"Composite Number")
+        break
+if x==1:
+    print("Neither Prime Nor Composite")
+
+if x>1000:
+    print("Do Not disturb")
+        
+if x%i!=0 and x<1000:
+    print(x,"Prime Number")
+
+    
