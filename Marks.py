c3d26df285accecdea58ded58336325dad988131
Rishabh Mehta <77108851+Productive-Rishabh@users.noreply.github.com>
2021-03-19 19:20:08 +0530

    Add files via upload

---
+++ Marks.py
@@ -0,0 +1,29 @@
+# If Else Program
+
+x = int(input("Please Enter Marks"))
+if x>100 or x<0:
+    print("Out of range")
+
+if x==100:
+    print("\nDarling aaj toh party hai")
+    
+        
+if x>=90 and x<100:
+    print("\nCongratcts you Got A Grade ")
+
+if x>=80 and x<90:
+    print("\nyou Got B Grade work little hard to get A Grade ")
+
+if x>=70 and x<80:
+     print("\nyou Got C Grade ")
+ 
+if x>=60 and x<70:
+     print("\nyou Got D Grade ")
+
+if x>50 and x<60:
+     print("\nyou Got E Grade ")
+
+if x<50 and x>=0:
+    print("fail Study at least to get pass")
+elif x==50:
+    print("Just Pass")
