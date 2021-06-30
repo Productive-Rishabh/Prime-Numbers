310548d0569eafa66ca77acbd30371bfd62f8f0f
Rishabh Mehta <77108851+Productive-Rishabh@users.noreply.github.com>
2021-06-30 13:31:08 +0530

    Add files via upload

---
+++ If else DL.py
@@ -0,0 +1,15 @@
+
+
+age=int(input("please enter age"))
+
+if age > 18 and age<100:
+    
+    print("show Driving Licence")
+    
+if age < 18 or age >100:
+    print("No Permit")
+        
+if age == 18:
+    print("Go For Driving Licence")
+
+    
