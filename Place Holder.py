3a57d5110b8a732c31cb734e19491ae6e184cc1b
Rishabh Mehta <77108851+Productive-Rishabh@users.noreply.github.com>
2021-06-30 11:12:58 +0530

    Add files via upload

---
+++ Place Holder.py
@@ -0,0 +1,5 @@
+p = str(input("Subject Name:- "))
+d = str(input("Your Name:- "))
+t = str(input("Topic Name:- "))
+
+print("Hi Welcome to tutorial of {}\nI Am Your teacher\n{}\nand out todays topic is\n{}".format(p,d,t))
